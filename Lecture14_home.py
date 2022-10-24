0adbc469a51d437346a8051f2dfeba752be1ca35
Kenny Fok <kfok@sandiego.edu>
2022-10-23 17:42:58 -0700

    Updating for F2022

--- Lecture14_home.py
+++ Lecture14_home.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot
 import Lecture14 as L14
 
-(depths, mags) = L14.get_data("eqs.csv")
+(depths, mags) = L14.get_data("Lecture14_eqs.csv")
 r_dm = L14.pearson_r(depths, mags)
 matplotlib.pyplot.title("Magnitude vs. Depth ({})".format(r_dm))
 matplotlib.pyplot.scatter(depths, mags)
